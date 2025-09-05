9ff4eb7b1ef5549fc2fd1052b7caceb958f46b9f
joaco <joacofiorina@hotmail.com>
2025-09-05 13:33:56 -0300

    prueba Render con html

---
+++ backend/src/main/java/com/hurryhand/backend/controllers/PruebaCloud.java
@@ -0,0 +1,15 @@
+package com.hurryhand.backend.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PruebaCloud {
+
+    @GetMapping("/test")
+    public String hurryhand() {
+        // Va a buscar el index.html que ya está en resources/static
+        return "forward:/pruebaCloud.html";
+    }
+}
--- backend/src/main/java/com/hurryhand/backend/models/Credential.java
+++ backend/src/main/java/com/hurryhand/backend/models/Credential.java
@@ -1,5 +1,5 @@
 package com.hurryhand.backend.models;
 
 public class Credential {
-    
+
 }
