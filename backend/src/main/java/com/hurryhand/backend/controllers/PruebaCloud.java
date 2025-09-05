23114c4e0986967796899f878d288a061723b832
joaco <joacofiorina@hotmail.com>
2025-09-05 14:00:18 -0300

    .

--- backend/src/main/java/com/hurryhand/backend/controllers/PruebaCloud.java
+++ backend/src/main/java/com/hurryhand/backend/controllers/PruebaCloud.java
@@ -1,15 +1,23 @@
 package com.hurryhand.backend.controllers;
 
-
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 @Controller
 public class PruebaCloud {
 
     @GetMapping("/test")
-    public String hurryhand() {
-        // Va a buscar el index.html que ya está en resources/static
-        return "redirect:/pruebaCloud.html";
+    public ResponseEntity<String> hurryhand() throws IOException {
+        var resource = new ClassPathResource("static/pruebaCloud.html");
+        String html = Files.readString(resource.getFile().toPath());
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .body(html);
     }
 }
