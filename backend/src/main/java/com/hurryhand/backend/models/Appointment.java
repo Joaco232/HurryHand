590284a06fb91858a0ab1edcfddcb2f0401c18c0
joaco <joacofiorina@hotmail.com>
2025-09-11 00:08:57 -0300

    arranque appointments

--- backend/src/main/java/com/hurryhand/backend/models/Appointment.java
+++ backend/src/main/java/com/hurryhand/backend/models/Appointment.java
@@ -1,4 +1,52 @@
 package com.hurryhand.backend.models;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Table(name = "APPOINTMENTS")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "PROVIDER_ID", nullable = false)
+    @NotNull(message = "El ID del proveedor no puede ser nulo.")
+    private Long providerId;
+
+    @Column(name = "SERVICE_ID", nullable = false)
+    @NotNull(message = "El ID del servicio no puede ser nulo.")
+    @ManyToOne
+    private ServicePost servicePostId;
+
+    @Column(name = "DATE_TIME", nullable = false)
+    @NotNull(message = "La fecha y hora no pueden ser nulas.")
+    @FutureOrPresent(message = "La fecha debe estar en el presente o futuro.")
+    private LocalDateTime dateTime;
+
+    @Column(name = "STATUS", nullable = false, length = 20)
+    @NotNull(message = "El estado no puede ser nulo.")
+    private String status;
+
+    @Column(name = "STATUS", nullable = false, length = 20)
+    @NotNull(message = "El estado del pago no puede ser nulo.")
+    private String paymentStatus;
+
+    // validar si tiene que ser un usuario y un service post o si se guarda las id nomas
+    @ManyToOne //
+    @NotNull(message = "El cliente no puede ser nulo.")
+    private User cliente;
+
 }
--- backend/src/main/java/com/hurryhand/backend/models/Provider.java
+++ backend/src/main/java/com/hurryhand/backend/models/Provider.java
@@ -2,11 +2,15 @@ package com.hurryhand.backend.models;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
 
 
 @Table(name = "PROVIDERS")
@@ -27,9 +31,9 @@ public class Provider {
     @JoinColumn(name = "USER_ID")
     private User user;
 
-
-
-
-
+    @Column(name = "RATING", precision = 2, scale = 1)
+    @DecimalMin(value = "0.0", inclusive = true, message = "El rating no puede ser menor a 0.0")
+    @DecimalMax(value = "5.0", inclusive = true, message = "El rating no puede ser mayor a 5.0")
+    private BigDecimal rating;
 
 }
