3dfb05cabd9d72f75aa9d65fc5e92e9bf765fb23
ElNelsonn <santbord@gmail.com>
2025-10-17 08:02:19 -0300

    GetByProvider

---
+++ backend/src/main/java/com/hurryhand/backend/dto/servicepost/ServicePostDTO.java
@@ -0,0 +1,41 @@
+package com.hurryhand.backend.dto.servicepost;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServicePostDTO {
+
+    private String title;
+
+    private String description;
+
+    private BigDecimal rating;
+
+    private Integer price;
+
+    private Integer duration;
+
+    private LocalDateTime createdAt;
+
+    private List<LocalDateTime> availableDates;
+
+    private List<String> photosURLs;
+
+
+
+
+
+
+
+}
