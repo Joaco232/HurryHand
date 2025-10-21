24227c64947586fcde60b2ba89b7d53fdc301644
joaco <joacofiorina@hotmail.com>
2025-10-21 16:54:22 -0300

    deploy back y DB postgres

--- backend/src/main/java/com/hurryhand/backend/services/ReviewService.java
+++ backend/src/main/java/com/hurryhand/backend/services/ReviewService.java
@@ -4,12 +4,18 @@ package com.hurryhand.backend.services;
 import com.hurryhand.backend.dto.review.CreateReviewDTO;
 import com.hurryhand.backend.mappers.ReviewMapper;
 import com.hurryhand.backend.models.Appointment;
+import com.hurryhand.backend.models.Review;
+import com.hurryhand.backend.models.ServicePost;
 import com.hurryhand.backend.models.User;
+import com.hurryhand.backend.repositories.AppointmentRepository;
 import com.hurryhand.backend.repositories.ReviewRepository;
+import com.hurryhand.backend.repositories.ServicePostRepository;
 import com.hurryhand.backend.validations.ReviewValidator;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +24,9 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final ReviewMapper reviewMapper;
     private final ReviewValidator reviewValidator;
+    private final ServicePostRepository servicePostRepository;
+    private final ServicePostService servicePostService;
+    private final AppointmentRepository appointmentRepository;
 
     @Transactional
     public void createReview(@Valid CreateReviewDTO createReviewDTO, Appointment appointment, User user) {
@@ -26,8 +35,17 @@ public class ReviewService {
 
         reviewValidator.UserAppointmentReview(appointment, user);
 
-        reviewRepository.save(reviewMapper.ToEntity(createReviewDTO, appointment));
+        Review review = reviewMapper.ToEntity(createReviewDTO, appointment);
 
+        reviewRepository.save(review);
+
+        appointment.setReview(review);
+
+        ServicePost servicePost = appointment.getServicePost();
+
+        servicePost.updateRating();
+
+        servicePostRepository.save(servicePost);
     }
 
 }
