27755d384d0e3410d24571df3b31b13d945d5fa3
joaco F. <fiorinamjoaquin@gmail.com>
2025-10-21 12:25:25 -0300

    railway

--- backend/src/main/java/com/hurryhand/backend/dto/editprofile/ChangePasswordRequestDTO.java
+++ backend/src/main/java/com/hurryhand/backend/dto/editprofile/ChangePasswordRequestDTO.java
@@ -12,7 +12,7 @@ public class ChangePasswordRequestDTO {
     @NotNull(message = "La contraseña no puede ser nula.")
     @Size(min = 8, max = 254, message = "La contraseña debe tener entre 8 y 254 caracteres.")
     private String currentPassword;
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?=\\S+$).{8,}$",
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-  zA-Z0-9])(?=\\S+$).{8,}$",
             message = "La contraseña debe contener al menos una letra mayúscula, una minúscula, un número y un carácter especial")
     @NotNull(message = "La contraseña no puede ser nula.")
     @Size(min = 8, max = 254, message = "La contraseña debe tener entre 8 y 254 caracteres.")
