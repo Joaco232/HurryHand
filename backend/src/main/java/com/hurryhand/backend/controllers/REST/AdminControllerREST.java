9fe8bff00a659804979ba9447ab11c29e9f52e85
joaco <joacofiorina@hotmail.com>
2025-10-28 15:14:28 -0300

    cambio pass

--- backend/src/main/java/com/hurryhand/backend/controllers/REST/AdminControllerREST.java
+++ backend/src/main/java/com/hurryhand/backend/controllers/REST/AdminControllerREST.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminControllerREST {
