b60b3cfb48ffeb55e3a32ad50e9b1cbf5eb090dc
joaco <joacofiorina@hotmail.com>
2025-11-07 12:47:07 -0300

    description crear service post

--- backend/src/main/java/com/hurryhand/backend/controllers/REST/ReviewControllerREST.java
+++ backend/src/main/java/com/hurryhand/backend/controllers/REST/ReviewControllerREST.java
@@ -1,10 +1,12 @@
 package com.hurryhand.backend.controllers.REST;
 
 import com.hurryhand.backend.dto.ApiResponse;
+import com.hurryhand.backend.dto.credential.CredentialResponseDTO;
 import com.hurryhand.backend.dto.review.CreateReviewDTO;
 import com.hurryhand.backend.dto.servicepost.CreateServicePostDTO;
 import com.hurryhand.backend.mappers.ApiResponseMapper;
 import com.hurryhand.backend.models.Appointment;
+import com.hurryhand.backend.models.Provider;
 import com.hurryhand.backend.models.User;
 import com.hurryhand.backend.security.CustomUserDetails;
 import com.hurryhand.backend.security.CustomUserDetailsService;
@@ -18,6 +20,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/review")
@@ -40,5 +44,8 @@ public class ReviewControllerREST {
         reviewService.createReview(reviewDTO, appointmentOfReview, activeUser);
         return apiResponseMapper.makeResponseEntity(HttpStatus.OK,"Review creada con exito");
     }
+
+
+
 
 }
